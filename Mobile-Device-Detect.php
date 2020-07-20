790bcda1873b397b3a6e053c98809c00d55d9f5a
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-07-20 07:00:34 -0400

    Delete Mobile-Device-Detect.php

--- Mobile-Device-Detect.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone')||strpos($_SERVER['HTTP_USER_AGENT'], 'iPad')){
-    Header("Location:https://www.google.com");
-}else{
-    Header("Location:https://www.duckduckgo.com");
-}
-?>
