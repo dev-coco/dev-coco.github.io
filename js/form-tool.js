ea52fde19c4396bf61f343cf4754de893d2d3159
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-11-28 11:32:40 -0400

    Update form-tool.js

--- js/form-tool.js
+++ js/form-tool.js
@@ -171,6 +171,173 @@
         google.script.run.cleaner();
         $(".post_link").val('');
     }
+
+    function getValue3() {
+        // 中文名字
+        var selectName = getSheetName("selectName").match(/[\u4e00-\u9fa5][\u4e00-\u9fa5]/g);
+        // 筛选后的名字
+        var filter_name = getSheetName("selectName").replace(/[0-9].+/g, "");
+        // 用户ID
+        var user_id = getSheetName("selectName").match(/[0-9][0-9]/g);
+        // 帐号名称
+        var fb_name = document.getElementsByClassName("account_name")[0].value.replace(/^ | $/g, "");
+        // 发帖位置
+        var fb_article_place = getSheetName("article_place");
+        // 发帖类型
+        var fb_post_type = getSheetName("post_type");
+        // 小组名称
+        var fb_place_name = document.getElementsByClassName("group_name")[0].value;
+        // 小组状态
+        var fb_group_status = getSheetName("group_status");
+        // 标题
+        var fb_title = document.getElementsByClassName("cn_title")[0].value;
+        //图片名称
+        var fb_image_name = document.getElementsByClassName("image_name")[0].value;
+        // 引导语
+        var fb_guide = document.getElementsByClassName("guide")[0].value;
+        // 中文引导语
+        var fb_cn_guide = document.getElementsByClassName("cn_guide")[0].value;
+        // 图片来源
+        var fn_image_origin = getSheetName("posting_status");
+        //网站原链接
+        var fn_web_url = document.getElementsByClassName("original_link")[0].value;
+        var fn_web_link1 = fn_web_url.replace(/\s/g, "");
+        var fn_web_link = fn_web_link1.replace(/\?source.+/g, "");
+        //发帖状态
+        var fn_posting_status = getSheetName("is_review");
+        //贴文链接
+        var fb_pose_link = document.getElementsByClassName("post_link")[0].value;
+        // 超文本链接
+        var fb_hyperlink = document.getElementsByClassName("materialize-textarea")[0].value;
+        //追踪链接
+        var track_link = document.getElementsByClassName("track_link")[0].value;
+        // 空值
+        var none = "";
+        // 检测
+        var detect = document.getElementsByClassName("btn waves-effect waves-teal one");
+
+        if (detect.length == 1) {
+            if (selectName == null, selectName == undefined) {
+                alert("请选择名字!");
+                return;
+            }
+            if (fb_name.length == 0) {
+                alert("请输入账号!");
+                return;
+            }
+            if (fb_post_type.length == 0) {
+                alert("请输入发帖类型!");
+                return;
+            }
+        } else {
+            if (selectName == null, selectName == undefined) {
+                alert("请选择名字!");
+                return;
+            }
+        }
+        var fb_post_url = fb_pose_link.match(/facebook\.com/g);
+        if (fb_pose_link.length == 0) {
+            alert("请输入贴文链接!");
+            return;
+        } else if (fb_post_url != "facebook.com") {
+            alert("请输入Facebook链接!");
+            return;
+        }
+        var d = new Date();
+        var a = d.getFullYear();
+        var a2 = d.getMonth() + 1
+        var a3 = d.getDate();
+        var date = a + "-" + a2 + "-" + a3;
+        var now_time = d.toLocaleTimeString();
+        document.getElementById("demo2").innerHTML = '发帖成功  ' + now_time;
+        google.script.run.userClicked4(date, selectName, filter_name, user_id, fb_name, fb_article_place, fb_post_type, fb_place_name, fb_group_status, fb_title, fb_image_name, fb_guide, fb_cn_guide, fn_image_origin, fn_web_link, fn_posting_status, fb_pose_link, track_link, fb_hyperlink, none);
+        google.script.run.userClicked5(date, selectName, filter_name, user_id, fb_name, fb_article_place, fb_post_type, fb_place_name, fb_group_status, fb_title, fb_image_name, fb_guide, fb_cn_guide, fn_image_origin, fn_web_link, fn_posting_status, fb_pose_link, track_link, fb_hyperlink, none);
+        google.script.run.cleaner();
+        $(".post_link").val('');
+    }
+    function getValue4() {
+        // 中文名字
+        var selectName = getSheetName("selectName").match(/[\u4e00-\u9fa5][\u4e00-\u9fa5]/g);
+        // 筛选后的名字
+        var filter_name = getSheetName("selectName").replace(/[0-9].+/g, "");
+        // 用户ID
+        var user_id = getSheetName("selectName").match(/[0-9][0-9]/g);
+        // 帐号名称
+        var fb_name = document.getElementsByClassName("account_name")[0].value.replace(/^ | $/g, "");
+        // 发帖位置
+        var fb_article_place = getSheetName("article_place");
+        // 发帖类型
+        var fb_post_type = getSheetName("post_type");
+        // 小组名称
+        var fb_place_name = document.getElementsByClassName("group_name")[0].value;
+        // 小组状态
+        var fb_group_status = getSheetName("group_status");
+        // 标题
+        var fb_title = document.getElementsByClassName("cn_title")[0].value;
+        //图片名称
+        var fb_image_name = document.getElementsByClassName("image_name")[0].value;
+        // 引导语
+        var fb_guide = document.getElementsByClassName("guide")[0].value;
+        // 中文引导语
+        var fb_cn_guide = document.getElementsByClassName("cn_guide")[0].value;
+        // 图片来源
+        var fn_image_origin = getSheetName("posting_status");
+        //网站原链接
+        var fn_web_url = document.getElementsByClassName("original_link")[0].value;
+        var fn_web_link1 = fn_web_url.replace(/\s/g, "");
+        var fn_web_link = fn_web_link1.replace(/\?source.+/g, "");
+        //发帖状态
+        var fn_posting_status = getSheetName("is_review");
+        //贴文链接
+        var fb_pose_link = document.getElementsByClassName("post_link")[0].value;
+        // 超文本链接
+        var fb_hyperlink = document.getElementsByClassName("materialize-textarea")[0].value;
+        //追踪链接
+        var track_link = document.getElementsByClassName("track_link")[0].value;
+        // 空值
+        var none = "";
+        // 检测
+        var detect = document.getElementsByClassName("btn waves-effect waves-teal one");
+
+        if (detect.length == 1) {
+            if (selectName == null, selectName == undefined) {
+                alert("请选择名字!");
+                return;
+            }
+            if (fb_name.length == 0) {
+                alert("请输入账号!");
+                return;
+            }
+            if (fb_post_type.length == 0) {
+                alert("请输入发帖类型!");
+                return;
+            }
+        } else {
+            if (selectName == null, selectName == undefined) {
+                alert("请选择名字!");
+                return;
+            }
+        }
+        var fb_post_url = fb_pose_link.match(/facebook\.com/g);
+        if (fb_pose_link.length == 0) {
+            alert("请输入贴文链接!");
+            return;
+        } else if (fb_post_url != "facebook.com") {
+            alert("请输入Facebook链接!");
+            return;
+        }
+        var d = new Date();
+        var a = d.getFullYear();
+        var a2 = d.getMonth() + 1
+        var a3 = d.getDate();
+        var date = a + "-" + a2 + "-" + a3;
+        var now_time = d.toLocaleTimeString();
+        document.getElementById("demo2").innerHTML = '发帖成功  ' + now_time;
+        google.script.run.userClicked6(date, selectName, filter_name, user_id, fb_name, fb_article_place, fb_post_type, fb_place_name, fb_group_status, fb_title, fb_image_name, fb_guide, fb_cn_guide, fn_image_origin, fn_web_link, fn_posting_status, fb_pose_link, track_link, fb_hyperlink, none);
+        google.script.run.userClicked7(date, selectName, filter_name, user_id, fb_name, fb_article_place, fb_post_type, fb_place_name, fb_group_status, fb_title, fb_image_name, fb_guide, fb_cn_guide, fn_image_origin, fn_web_link, fn_posting_status, fb_pose_link, track_link, fb_hyperlink, none);
+        google.script.run.cleaner();
+        $(".post_link").val('');
+    }
     // 获取index
     function getSheetName(selectID) {
         //1:拿到select对象
