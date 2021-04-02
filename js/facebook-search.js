c0f7f737a3148980a996d4ae2f06220641032973
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-04-02 10:54:04 -0400

    Add files via upload

---
+++ js/facebook-search.js
@@ -0,0 +1,25 @@
+var website = "https://dev-coco.github.io/Online-Tools/Facebook-Site-Search/";
+
+function India_Facebook() {
+    document.getElementById("devices").src = website + "India/India-Facebook.html"
+}
+
+function India_Facebook_Group() {
+    document.getElementById("devices").src = website + "India/India-Facebook-Group.html"
+}
+
+function India_Facebook_about() {
+    document.getElementById("devices").src = website + "India/India-Facebook-about.html"
+}
+
+function India_Facebook_pages() {
+    document.getElementById("devices").src = website + "India/India-Facebook-pages.html"
+}
+
+function India_Facebook_community() {
+    document.getElementById("devices").src = website + "India/India-Facebook-community.html"
+}
+
+function India_Facebook_places() {
+    document.getElementById("devices").src = website + "India/India-Facebook-places.html"
+}
