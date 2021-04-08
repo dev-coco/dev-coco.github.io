3c7dd061fd6d8996c3a8faf9fbfb4f7451c92331
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-04-08 00:58:26 -0400

    Update facebook-search.js

--- js/facebook-search.js
+++ js/facebook-search.js
@@ -23,3 +23,27 @@ function India_Facebook_community() {
 function India_Facebook_places() {
     document.getElementById("devices").src = website + "India/India-Facebook-places.html"
 }
+// ------------------------------------------------------------------
+function Global_Facebook() {
+    document.getElementById("devices").src = website + "Global/Global-Facebook.html"
+}
+
+function Global_Facebook_Group() {
+    document.getElementById("devices").src = website + "Global/Global-Facebook-Group.html"
+}
+
+function Global_Facebook_about() {
+    document.getElementById("devices").src = website + "Global/Global-Facebook-about.html"
+}
+
+function Global_Facebook_pages() {
+    document.getElementById("devices").src = website + "Global/Global-Facebook-pages.html"
+}
+
+function Global_Facebook_community() {
+    document.getElementById("devices").src = website + "Global/Global-Facebook-community.html"
+}
+
+function Global_Facebook_places() {
+    document.getElementById("devices").src = website + "Global/Global-Facebook-places.html"
+}
