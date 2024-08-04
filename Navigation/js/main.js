da10771375d45ca5a434b94477ea55786619f850
Raz1ner <63853884+dev-coco@users.noreply.github.com>
2024-08-03 21:29:02 -0400

    update

--- Navigation/js/main.js
+++ Navigation/js/main.js
@@ -29,7 +29,6 @@ $(document).ready(() => {
   })
   $('a.smooth').click(function (ev) {
     ev.preventDefault()
-
     public_vars.$mainMenu.add(public_vars.$sidebarProfile).toggleClass('mobile-is-visible')
     ps_destroy()
     $('html, body').animate({
@@ -43,17 +42,18 @@ $(document).ready(() => {
 })
 
 const searchList = {
+  'type-felo': 'https://www.felo.ai/search?q=',
   'type-google': 'https://www.google.com/search?q=',
   'type-youtube': 'https://www.youtube.com/results?search_query=',
   'type-github': 'https://github.com/search?q=',
   'type-bing': 'https://www.bing.com/search?q=',
   'type-duckduckgo': 'https://duckduckgo.com/?q=',
-  'type-startpage': 'https://www.startpage.com/sp/search?query=',
+  'type-startpage': 'https://www.startpage.com/sp/search?query='
 }
 const keywordSearch = document.getElementById('keyword-search')
 const searchText = document.getElementById('search-text')
 
-keywordSearch.addEventListener('click', () => {
+function textSearch () {
   const typeID = document.querySelector('input[name="type"]:checked').id
   const text = searchText.value.trim()
   let link = ''
@@ -63,4 +63,13 @@ keywordSearch.addEventListener('click', () => {
     link = searchList[typeID].replace(/(?<=\b)\/.+/g, '')
   }
   window.open(link, '_blank')
+}
+
+keywordSearch.addEventListener('click', textSearch)
+
+searchText.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    textSearch()
+  }
 })
